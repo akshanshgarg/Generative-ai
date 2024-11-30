30c37fd6bd4b90d284bc0379d20c52e3faac064a
Akshansh Garg <akshanshgarg18@gmail.com>
2024-11-30 19:03:48 +0530

    Used llama2 and openai llm

--- chatbot/localama.py
+++ chatbot/localama.py
@@ -1,13 +1,13 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-
+from langchain_community.llms import Ollama
 import streamlit as st
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 ## Langmith tracking
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
@@ -22,18 +22,19 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
+
 # streamlit framework
 
-st.title('Langchain demo with Openai API')
+st.title('Langchain demo with Llama2 API')
 input_text = st.text_input("Search the topic you want")
 
-# Open ai LLM
+# llama2 LLM
 
-# llm = ChatOpenAI(model="gpt-3.5-turbo")
-llm = ChatOpenAI()
+llm = Ollama(model = "llama2")
 output_parser = StrOutputParser()
 
 chain = prompt|llm|output_parser
 
 if input_text:
     st.write(chain.invoke({'question': input_text}))
+
