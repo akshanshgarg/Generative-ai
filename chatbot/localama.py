e7e18570b14a1ea57bd5a350dbd98e4b7a8cf5e5
Akshansh Garg <akshanshgarg18@gmail.com>
2024-11-30 19:32:28 +0530

    used updated version ollama

--- chatbot/localama.py
+++ chatbot/localama.py
@@ -1,7 +1,8 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.llms import Ollama
+# from langchain_community.llms import Ollama
+from langchain_ollama import OllamaLLM
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -30,7 +31,7 @@ input_text = st.text_input("Search the topic you want")
 
 # llama2 LLM
 
-llm = Ollama(model = "llama2")
+llm = OllamaLLM(model = "llama2")
 output_parser = StrOutputParser()
 
 chain = prompt|llm|output_parser
